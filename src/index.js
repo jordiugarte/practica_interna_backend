cd5b92bbc4504bbe24613b76a6f96ef71f6c8eb5
Jordi Ugarte <jordi933@gmail.com>
2022-01-24 15:37:50 -0400

    los pajings

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const port = 7900;
 
 mongoose.Promise = global.Promise;
 
-/*Servidor*///mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
-/*Local*/mongoose.connect('mongodb://localhost:27017/jugarte18', {
+mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
+//mongoose.connect('mongodb://localhost:27017/jugarte18', {
     useNewUrlParser: true,
     useUnifiedTopology: true
     })
