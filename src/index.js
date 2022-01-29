08f9d679f0a733b0fbb59bfbd226696fd20e3405
jonicaconstrucciones <96605544+jonicaconstrucciones@users.noreply.github.com>
2022-01-28 23:25:30 -0400

    ssl

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18?ssl=true', {
 //mongoose.connect('mongodb://localhost:27017/jugarte18', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    sslValidate: true
+    useUnifiedTopology: true
     })
     .then(() => {
         console.log("Conexion a la base de datos establecida con exito");
