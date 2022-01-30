061de2a4c8e8a72c412124b973cc2ff7666d700e
jonicaconstrucciones <96605544+jonicaconstrucciones@users.noreply.github.com>
2022-01-30 18:06:39 -0400

    key

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ssl: true,
-    sslValidate: true,
+    sslValidate: false,
     sslCA: './src/key.pem'
 })
     .then(() => {
