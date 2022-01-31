75d6dce2c82c66f7aa855250f25ea3cfb0970eed
jonicaconstrucciones <96605544+jonicaconstrucciones@users.noreply.github.com>
2022-01-31 18:36:01 -0400

    primera prueba con ssl

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
     //mongoose.connect('mongodb://localhost:27017/jugarte18', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true,
-    sslCA: './src/key.pem'
+    useUnifiedTopology: true
 })
     .then(() => {
         console.log("Conexion a la base de datos establecida con exito");
