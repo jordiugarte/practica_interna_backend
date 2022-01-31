767b17e51463349a21679850ed56695ba9faa053
jonicaconstrucciones <96605544+jonicaconstrucciones@users.noreply.github.com>
2022-01-31 19:34:22 -0400

    alv

--- src/index.js
+++ src/index.js
@@ -2,24 +2,17 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const port = 7900;
 
-const fs = require('fs');
-const https = require('https');
-const req = require('express/lib/request');
-
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
-    mongoose.connect('mongodb://localhost:27017/jugarte18', {
+mongoose.connect('mongodb://jugarte18:j0rd12022@localhost:27017/jugarte18', {
+    //mongoose.connect('mongodb://localhost:27017/jugarte18', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(() => {
         console.log("Conexion a la base de datos establecida con exito");
-        https.createServer({
-            cert: fs.readFileSync('selfsigned.crt'),
-            key: fs.readFileSync('selfsigned.key')
-        }, app).listen(port, () => {
-            console.log('SSL/TLS Server using self generated certificate');
+        console.log('SSL/TLS Server using self generated certificate');
+        app.listen(port, () => {
             console.log(`Servidor corriendo correctamente en la url: localhost:${port}`);
         });
     })
